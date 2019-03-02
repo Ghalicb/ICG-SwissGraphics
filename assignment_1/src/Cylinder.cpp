28e0e1407ec7b6af03cce32c65c9b505a382cbf1
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-03-03 00:20:04 +0100

    Add working cylinder intersect code BUT NEEDS REVIEW

--- assignment_1/src/Cylinder.cpp
+++ assignment_1/src/Cylinder.cpp
@@ -34,20 +34,48 @@ intersect(const Ray&  _ray,
      * - return whether there is an intersection with t > 0
     */
 
-    vec3 delta_points = _ray.origin - center;
-    double dot_ray_dir_axis = dot(_ray.direction, axis);
-    vec3 dot_delta_axis_times_axis = dot(delta_points, axis)*axis;
+    // precompute redundant expressions for a, b and c
+    vec3 delta_points                = _ray.origin - center;
+    vec3 dot_ray_dir_axis_times_axis = dot(_ray.direction, axis) * axis;
+    vec3 dot_delta_axis_times_axis   = dot(delta_points, axis)*axis;
 
-    vec3 a_sqrt = (_ray.direction - dot_ray_dir_axis* axis);
+    vec3 a_sqrt = (_ray.direction - dot_ray_dir_axis_times_axis);
     double a = dot(a_sqrt, a_sqrt);
 
-    double b = 2 * dot(_ray.direction - dot_ray_dir_axis * axis,
-                       dot_delta_axis_times_axis);
+    double b = 2 * dot(_ray.direction - dot_ray_dir_axis_times_axis,
+                       delta_points - dot_delta_axis_times_axis);
 
     vec3 c_minus_r2_sqrt = delta_points - dot_delta_axis_times_axis;
     double c = dot(c_minus_r2_sqrt, c_minus_r2_sqrt) - radius*radius;
 
-    //solveQuadratic(a, b, c, );
+    std::array<double, 2> t;
+    size_t nsol = solveQuadratic(a, b, c, t);
 
-    return false;
+    _intersection_t = NO_INTERSECTION;
+
+    for (size_t i = 0; i < nsol; ++i)
+    {
+        //check the solution is in font of the viewer,
+        //closer than the others and does not exceed the height
+        if (t[i] > 0 && t[i] < _intersection_t
+                     && abs(dot(axis * height, _ray(t[i]) - center)) < 1)
+            _intersection_t = std::min(_intersection_t, t[i]);
+    }
+
+    if (_intersection_t == NO_INTERSECTION)
+        return false;
+
+    _intersection_point  = _ray(_intersection_t);
+    //compute the normal as
+    //the intersection point minus the orthogonal projection
+    //of the intersection point on axis
+    _intersection_normal = normalize(_intersection_point-
+        (center + dot(_intersection_point - center, axis)*axis));
+
+    if (dot(_ray.direction, _intersection_normal) > 0)
+    {
+        _intersection_normal *= -1;
+    }
+
+    return true;
 }
