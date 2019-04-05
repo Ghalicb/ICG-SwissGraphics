710bf14e8affdf43948e80382ba8e677a9729b86
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-04-05 16:38:33 +0200

    Change to GL_ONE

--- Rasterisation/src/solar_viewer.cpp
+++ Rasterisation/src/solar_viewer.cpp
@@ -544,7 +544,7 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
     mv_matrix = _view * m_matrix;
     mvp_matrix = _projection * mv_matrix;
     glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_SRC_COLOR);
+    glBlendFunc(GL_ONE, GL_ONE);
     color_shader_.use();
     color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
     sunglow_.tex_.bind();
