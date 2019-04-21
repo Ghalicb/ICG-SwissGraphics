0af938cf0f7602027b43e11e06c0124560e2b19e
Ghali C <ghalicb@gmail.com>
2019-04-21 18:43:12 +0200

    [Task 2.1] implement draw

--- Lindenmayer/src/lsystem.cpp
+++ Lindenmayer/src/lsystem.cpp
@@ -80,8 +80,52 @@ std::vector<Segment> LindenmayerSystem::draw(std::string const& symbols) {
 		Build line segments according to the sequence of symbols
 		The initial position is (0, 0) and the initial direction is "up" (0, 1)
 		Segment is std::pair<vec2, vec2>
+		
 	*/
-	return {};
+	std::vector<Segment> lines;
+	vec2 position = vec2(0,0);
+	vec2 direction = vec2(0,1);
+
+	// Stack to keep in memory positions & directions when using []
+	std::vector<vec2> positions_stack;
+	std::vector<vec2> directions_stack;
+
+
+	double radii_angle = deg2rad(rotation_angle_deg);
+	// Rotation matrix for +
+	mat2 rotation_plus = mat2(cos(radii_angle), -sin(radii_angle), 
+								sin(radii_angle), cos(radii_angle));
+	// Rotation matrix for -
+	mat2 rotation_minus = mat2(cos(radii_angle), sin(radii_angle), 
+								-sin(radii_angle), cos(radii_angle));
+
+	for (const char symbol : symbols) {
+
+		switch (symbol) {
+			case '+' :
+				direction = rotation_plus * direction;
+				break;
+			case '-' :
+				direction = rotation_minus * direction;
+				break;
+			case '[' :
+				positions_stack.push_back(position);
+				directions_stack.push_back(direction);
+				break;
+			case ']' :
+				position = positions_stack.back();
+				direction = directions_stack.back();
+				positions_stack.pop_back();
+				directions_stack.pop_back();
+				break;
+			default :
+				vec2 new_position = position + direction; // Knowing that direction is always a unit vector
+				lines.push_back(Segment(position, new_position));
+				position = new_position;
+				break;
+		}
+	}
+	return lines;
 	
 	//============================================================
 }
