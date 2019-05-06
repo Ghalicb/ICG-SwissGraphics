81b78e4bfe39b87c8ae48f2560deecf122dc4aac
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-05-06 20:45:24 +0200

    Fix bugs in cuboid and implement rotation

--- Project/Improved-Ray-Tracer/src/Cuboid.cpp
+++ Project/Improved-Ray-Tracer/src/Cuboid.cpp
@@ -7,6 +7,8 @@ intersect(const Ray&  _ray,
                vec3&  _intersection_normal,
              double&  _intersection_t) const
 {
+  _intersection_t = NO_INTERSECTION;
+
   int solutions = 0;
 
   for (size_t i = 0; i < faces.size(); ++i)
@@ -16,64 +18,59 @@ intersect(const Ray&  _ray,
 
     vec3   intersection_point;
     vec3   intersection_normal;
-    double intersection_t  = NO_INTERSECTION;
-
-    double _intersection_t = NO_INTERSECTION;
+    double intersection_t;
 
     if (f.intersect(_ray, intersection_point, intersection_normal, intersection_t))
     {
       vec3 center_to_point = intersection_point - f.getCenter();
-      bool in_cuboid = false;
 
-      double dot_ctp_x_axis;
-      double dot_ctp_y_axis;
-      double dot_ctp_z_axis;
-
-      switch (i) {
-        case 0:
-        case 1:
-          dot_ctp_y_axis = dot(center_to_point, y_axis);
-          dot_ctp_z_axis = dot(center_to_point, z_axis);
-          if (dot_ctp_y_axis < y_size/2 &&
-              dot_ctp_z_axis < z_size/2)
-            in_cuboid = true;
-          break;
-        case 2:
-        case 3:
-          dot_ctp_x_axis = dot(center_to_point, x_axis);
-          dot_ctp_z_axis = dot(center_to_point, z_axis);
-          if (dot_ctp_x_axis < x_size/2 &&
-              dot_ctp_z_axis < z_size/2)
-            in_cuboid = true;
-          break;
-        case 4:
-        case 5:
-          dot_ctp_x_axis = dot(center_to_point, x_axis);
-          dot_ctp_y_axis = dot(center_to_point, y_axis);
-          if (dot_ctp_x_axis < x_size/2 &&
-              dot_ctp_y_axis < y_size/2)
-            in_cuboid = true;
-          break;
-      }
-
-      if (in_cuboid && _intersection_t < _intersection_t)
+      if (norm(center_to_point) <= max_size)
       {
-        _intersection_point  = intersection_point;
-        _intersection_normal = intersection_normal;
-        _intersection_t      = intersection_t;
+        bool in_cuboid = false;
 
-        ++solutions;
+        double dot_ctp_x_axis;
+        double dot_ctp_y_axis;
+        double dot_ctp_z_axis;
+
+        switch (i) {
+          case 0:
+          case 1:
+            dot_ctp_y_axis = fabs(dot(center_to_point, y_axis));
+            dot_ctp_z_axis = fabs(dot(center_to_point, z_axis));
+            if (dot_ctp_y_axis <= y_size/2 && dot_ctp_z_axis <= z_size/2) {in_cuboid = true; }
+            break;
+          case 2:
+          case 3:
+            dot_ctp_x_axis = fabs(dot(center_to_point, x_axis));
+            dot_ctp_z_axis = fabs(dot(center_to_point, z_axis));
+            if (dot_ctp_x_axis <= x_size/2 && dot_ctp_z_axis <= z_size/2) { in_cuboid = true; }
+            break;
+          case 4:
+          case 5:
+            dot_ctp_x_axis = fabs(dot(center_to_point, x_axis));
+            dot_ctp_y_axis = fabs(dot(center_to_point, y_axis));
+            if (dot_ctp_x_axis <= x_size/2 && dot_ctp_y_axis <= y_size/2) { in_cuboid = true; }
+            break;
+        }
+
+        if (in_cuboid && intersection_t > 0 && intersection_t < _intersection_t)
+        {
+          _intersection_point  = intersection_point;
+          _intersection_normal = intersection_normal;
+          _intersection_t      = intersection_t;
+
+          ++solutions;
+        }
       }
     }
   }
 
-  std::cout << solutions;
-  if (solutions > 0)
+  if (solutions <= 0 || _intersection_t >= NO_INTERSECTION)
   {
-    return true;
+    return false;
   }
   else
   {
-    return false;
+    return true;
   }
 }
--- Project/Improved-Ray-Tracer/src/Cuboid.h
+++ Project/Improved-Ray-Tracer/src/Cuboid.h
@@ -6,27 +6,22 @@
 #include "vec3.h"
 
 #include <array>
+#include <math.h>
+
+#define PI 3.14159265
 
 class Cuboid : public Object
 {
 public:
-  Cuboid(const vec3 &_center = vec3(0,0,0),
-         const vec3 &_x_axis = vec3(1,0,0),
-         const vec3 &_y_axis = vec3(0,1,0),
-              double _x_size = 1,
+  Cuboid(const vec3 &_center=vec3(0,0,0),
+              double _x_size=1,
               double _y_size = 1,
-              double _z_size = 1): center(_center), x_axis(_x_axis), y_axis(_y_axis),
-                                   x_size(_x_size), y_size(_y_size), z_size(_z_size)
-  {
-    z_axis = normalize(cross(x_axis, y_axis));
-
-    faces[0] = Plane(center + vec3(x_size/2, 0, 0),  x_axis);
-    faces[1] = Plane(center - vec3(x_size/2, 0, 0), -x_axis);
-    faces[2] = Plane(center + vec3(0, y_size/2, 0),  y_axis);
-    faces[3] = Plane(center - vec3(0, y_size/2, 0), -y_axis);
-    faces[4] = Plane(center + vec3(0, 0, z_size/2),  z_axis);
-    faces[5] = Plane(center - vec3(0, 0, z_size/2), -z_axis);
-  }
+              double _z_size = 1,
+              double _rotation_angle=0):center(_center),
+                                        x_size(_x_size),
+                                        y_size(_y_size),
+                                        z_size(_z_size),
+                                        rotation_angle(_rotation_angle){}
 
   Cuboid(std::istream &is) { parse(is); }
 
@@ -37,21 +32,39 @@ public:
 
   virtual void parse(std::istream &is) override
   {
-    is >> center >> x_axis >> y_axis >> x_size >> y_size >> z_size >> material;
-    x_axis = normalize(x_axis);
-    y_axis = normalize(y_axis);
+    is >> center >> x_size >> y_size >> z_size >> rotation_angle >> material;
+
+    max_size = norm(vec3(x_size/2, y_size/2, z_size/2));
+
+    double c = cos(rotation_angle * PI / 180.0);
+    double s = sin(rotation_angle * PI / 180.0);
+
+    x_axis = vec3( c,0,s);
+    y_axis = vec3( 0,1,0);
+    z_axis = vec3(-s,0,c);
+
+    faces[0] = Plane(center + x_axis*x_size/2,  x_axis);
+    faces[1] = Plane(center - x_axis*x_size/2, -x_axis);
+    faces[2] = Plane(center + y_axis*y_size/2,  y_axis);
+    faces[3] = Plane(center - y_axis*y_size/2, -y_axis);
+    faces[4] = Plane(center + z_axis*z_size/2,  z_axis);
+    faces[5] = Plane(center - z_axis*z_size/2, -z_axis);
   }
 
 private:
   vec3 center;
 
+  double x_size;
+  double y_size;
+  double z_size;
+
   vec3 x_axis;
   vec3 y_axis;
   vec3 z_axis;
 
-  double x_size;
-  double y_size;
-  double z_size;
+  double rotation_angle;
+
+  double max_size;
 
   std::array<Plane, 6> faces;
 };
