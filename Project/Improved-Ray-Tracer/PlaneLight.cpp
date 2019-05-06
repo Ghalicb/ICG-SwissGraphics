8ef23c8075cb0aea71bd64f8cfa305e79ca7c8b1
Samuel Chassot <samuel.chassot@epfl.ch>
2019-05-06 08:38:47 +0200

    continue working on plane light

---
+++ Project/Improved-Ray-Tracer/PlaneLight.cpp
@@ -0,0 +1,13 @@
+//
+//  PlaneLight.cpp
+//  raytrace
+//
+//  Created by Samuel Chassot on 05.05.19.
+//
+
+#include <stdio.h>
+#include "src/PlaneLight.h"
+
+
+//== CLASS DEFINITION =========================================================
+PlaneLight::PlaneLight()
--- Project/Improved-Ray-Tracer/src/PlaneLight.h
+++ Project/Improved-Ray-Tracer/src/PlaneLight.h
@@ -12,24 +12,43 @@
 
 //== CLASS DEFINITION =========================================================
 
-/// The class represents a surface light source and stores position and color and shape.
-struct PlaneLight
+class PlaneLight : public Object
 {
-    Light(std::istream &is) { is >> center_position >> normal >> size >> color; }
+public:
+    /// constructor
+    PlaneLight(const vec3& _center=vec3(0,0,0), const vec3& _normal=vec3(0,1,0), const double _size, const vec3& _color);
     
-    /// position of the center of the plane light source
-    vec3 center_position;
+    /// Construct a plane light with parameters parsed from an input stream.
+    PlaneLight(std::istream &is) { parse(is); }
     
-    /// normal of the plane light
+    /// Compute the intersection of the plane light with \c _ray. Return whether
+    /// there is an intersection. If there is one, return the intersection data.
+    /// This function overrides Object::intersect().
+    /// \param[in] _ray the ray to intersect the planeLight with
+    /// \param[out] _intersection_point position of the intersection
+    /// \param[out] _intersection_normal normal vector at the intersection point
+    /// \param[out] _intersection_t ray parameter at the intesection point
+    virtual bool intersect(const Ray&  _ray,
+                           vec3&       _intersection_point,
+                           vec3&       _intersection_normal,
+                           double&     _intersection_t) const override;
+    
+    /// parse plane from an input stream
+    virtual void parse(std::istream &is) override {
+        is >> center >> normal >> size >> color;
+        normal = normalize(normal);
+    }
+    
+private:
+    /// center point on the plane light
+    vec3 center;
+    /// normal vector of the plane light
     vec3 normal;
-    
-    /// size of a side of the plane light
+    /// size of a side of the square plane light
     double size;
-    
-    /// color of the light source
+    /// color of the light
     vec3 color;
 };
-
 
 //-----------------------------------------------------------------------------
 
@@ -40,8 +59,3 @@ inline std::istream& operator>>(std::istream& is, Light& l)
     l = Light(is);
     return is;
 }
-
-
-//=============================================================================
-#endif // LIGHT_H defined
-//=============================================================================
