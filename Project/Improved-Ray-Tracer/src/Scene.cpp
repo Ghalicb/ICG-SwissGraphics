b48390e0d82c34ecec32a6d5476247db1dc3a3c0
Samuel Chassot <samuel.chassot@epfl.ch>
2019-05-07 09:15:49 +0200

    do random ray tracing only when not directly lit

--- Project/Improved-Ray-Tracer/src/Scene.cpp
+++ Project/Improved-Ray-Tracer/src/Scene.cpp
@@ -30,7 +30,7 @@
 #include <tbb/parallel_for.h>
 #endif
 
-# define PATHS_PER_PIXEL 50
+# define PATHS_PER_PIXEL 100
 //-----------------------------------------------------------------------------
 
 Image Scene::render()
@@ -195,17 +195,19 @@ vec3 Scene::lighting(const vec3& _point, const vec3& _normal, const vec3& _view,
             if (dot_reflection_light_view > 0) {
                 specular += light.color * _material.specular * pow(dot_reflection_light_view, _material.shininess);
             }
-        }
+        } else {
+          // Path tracing part
+          if (dot_normal_light > 0) {
+            //diffuse objects
+            vec3 random_reflected_ray = normalize(vec3(rand()%10, rand()%10, rand()%10));
+            random_reflected_ray = dot(random_reflected_ray, _normal) < 0 ? -random_reflected_ray : random_reflected_ray;
+            Ray reflected_ray = Ray(_point + EPSILON * _normal, random_reflected_ray);
 
-        // Path tracing part
-        if (dot_normal_light > 0) {
-          //diffuse objects
-          vec3 random_reflected_ray = normalize(vec3(rand()%10, rand()%10, rand()%10));
-          random_reflected_ray = dot(random_reflected_ray, _normal) < 0 ? -random_reflected_ray : random_reflected_ray;
-          Ray reflected_ray = Ray(_point + EPSILON * _normal, random_reflected_ray);
+            vec3 color_traced = trace(reflected_ray, _depth + 1);
+            diffuse += diffuse*0.7 + 0.3*color_traced;
 
-          diffuse += 0.1*trace(reflected_ray, _depth+1);
-        }
+          }
+      }
 
     }
 
