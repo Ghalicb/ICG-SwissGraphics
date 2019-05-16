6c29e3baa07ba2742c432466f62ff8f8cf2b545d
Ghali C <ghalicb@gmail.com>
2019-05-16 15:11:02 +0200

    Add the interface of the ClosedCylinder class

---
+++ Project/Improved-Ray-Tracer/src/ClosedCylinder.h
@@ -0,0 +1,50 @@
+#ifndef CLOSED_CYLINDER_H
+#define CLOSED_CYLINDER_H
+
+//== INCLUDES =================================================================
+
+#include "Object.h"
+#include "Cylinder.h"
+#include "Plane.h"
+#include "vec3.h"
+
+//== CLASS DEFINITION =========================================================
+
+
+/// \class ClosedCylinder Cylinder.h
+/// This class implements a closed cylinder object, which is specified by one
+/// open cylinder and two closed planes.
+class ClosedCylinder : public Object {
+    public:
+        /// Construct a closed cylinder by directly specifying its parameters
+        ClosedCylinder(const vec3 &_center = vec3(0,0,0),
+                double _radius = 1,
+                const vec3 &_axis = vec3(1,0,0),
+                double _height = 1)
+            :  cylinder(_center,_radius,_axis,_height), 
+                upperPlane(_center + _height/2.*_axis, _axis),
+                bottomPlane(_center - _height/2*_axis, -_axis) { }
+
+        /// Construct a closed cylinder with parameters parsed from an input stream.
+        ClosedCylinder(std::istream &is) { parse(is); }
+
+        virtual bool intersect(const Ray&  _ray,
+                           vec3&       _intersection_point,
+                           vec3&       _intersection_normal,
+                           double&     _intersection_t) const override;
+
+        /// parse cylinder from an input stream
+        virtual void parse(std::istream &is) override {
+            cylinder.parse(is);
+            upperPlane.parse(is);
+            bottomPlane.parse(is);
+        }
+
+    private:
+        Cylinder cylinder;
+        Plane upperPlane;
+        Plane bottomPlane;
+};
+
+
+#endif
