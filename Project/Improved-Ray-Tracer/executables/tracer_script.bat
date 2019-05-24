13738988a05613c2c909cc2154b50e3b0436c4e9
Samuel Chassot <samuel.chassot@epfl.ch>
2019-05-24 15:33:07 +0200

    add script for windows

---
+++ Project/Improved-Ray-Tracer/executables/tracer_script.bat
@@ -0,0 +1,7 @@
+raytrace.exe scenes\caustics_stack\caustics_stack.sce results\caustics_stack.png
+raytrace.exe scenes\cornell_boxes\cornell_box_1.sce results\cornell_box_1.png
+raytrace.exe scenes\cornell_boxes\cornell_box_2.sce results\cornell_box_2.png
+raytrace.exe scenes\cornell_boxes\cornell_box_3.sce results\cornell_box_3.png
+raytrace.exe scenes\empty_prison\empty_prison.sce results\empty_prison.png
+raytrace.exe scenes\glass_ball\glass_ball.sce results\glass_ball.png
+raytrace.exe scenes\glass_w_straw\glass_w_straw.sce results\glass_w_straw.png
